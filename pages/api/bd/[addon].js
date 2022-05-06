682cc890f0d9e0719b533e4d3ddc942cce558b1a
Taimoor Tariq <45585081+Taimoor-Tariq@users.noreply.github.com>
2022-05-06 12:39:40 -0400

    Update API

---
+++ pages/api/bd/[addon].js
@@ -0,0 +1,20 @@
+import stream from "stream";
+import { promisify } from "util";
+import fetch from "node-fetch";
+
+const pipeline = promisify(stream.pipeline);
+
+const handler = async (req, res) => {
+    const { addon } = req.query;
+    const filename = addon,
+        response = await fetch(`https://raw.githubusercontent.com/Taimoor-Tariq/BetterDiscordStuff/main/${filename.split(".")[1] == "plugin" ? "Plugins" : "Themes"}/${filename.split(".")[0]}/${filename}`);
+
+    if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
+
+    res.setHeader("Content-Type", "text/javascript");
+    res.setHeader("Content-Disposition", `attachment; filename="${filename}"`);
+    await pipeline(response.body, res);
+    res.end();
+};
+
+export default handler;
