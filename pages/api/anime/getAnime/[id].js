34178ffb2553c2c61b2fe1873717be48ce736038
Riftwalker Studios <45585081+Riftwalker-Studios@users.noreply.github.com>
2023-01-23 15:44:25 -0500

    Removed anime API

--- pages/api/anime/getAnime/[id].js
+++
@@ -1,8 +0,0 @@
-const { META } = require('@consumet/extensions');
-const anilist = new META.Anilist();
-
-export default async function handler(req, res) {
-    const { id } = req.query;
-    const anime = await anilist.fetchAnimeInfo(id);
-    res.status(200).json(anime);
-}
--- pages/api/anime/searchAnime/[query].js
+++
@@ -1,8 +0,0 @@
-const { META } = require('@consumet/extensions');
-const anilist = new META.Anilist();
-
-export default async function handler(req, res) {
-    const { query } = req.query;
-    const { results } = await anilist.search(query);
-    res.status(200).json(results);
-}
